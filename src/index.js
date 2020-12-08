cf52d5a1b016984784867fffb0fd9a00ba262f99
Oxana Odegova <oxyapple@yandex.ru>
2020-12-08 20:23:29 +0100

    init commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// import React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a React Component
+const App = () => {
+    return <div>Hi there!</div>
+};
+
+// Take the react component and show it on the screen
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
